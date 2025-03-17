93a3f75adc18497d03c9d533c09310dc289e1ca2
ycwwang <ycwwang>
2025-03-17 16:10:58 +0800

    Version 0.0.0

--- R/app_ui.R
+++ R/app_ui.R
@@ -4,13 +4,87 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
+
+# ui.R
+library(tidyverse)
+library(lubridate)
+library(collapse)
+library(data.table)
+library(openxlsx)
+library(dataRetrieval)
+library(shiny)
+library(shinybusy)
+library(shinyWidgets)
+library(shinyjs)
+library(shinydashboard)
+library(shinyalert)
+library(DT)
+library(sf)
+library(leaflet)
+library(leaflet.extras)
+library(plotly)
+library(zip)
+library(EPATADA)
+library(tigris)
+library(tidycensus)
+library(spsComps)
+library(scales)
+
+# Set the upload size to 5MB
+mb_limit <- 10000
+options(shiny.maxRequestSize = mb_limit * 1024^2)
+
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      golem::golem_welcome_page() # Remove this line to start building your UI
+    dashboardPage(
+      # The header panel
+      header = dashboardHeader(title = "WQP Data Tool"),
+      # The sidebar panel
+      sidebar = dashboardSidebar(
+        # Use Shiny Busy
+        add_busy_spinner(spin = "circle", position = "top-right"),
+        useSweetAlert(),
+
+        # Sidebar menu
+        sidebarMenu(
+          id = "tabs",
+          menuItem(
+            text = "Data Download",
+            tabName = "Download",
+            icon = icon("pen")
+          )
+        )
+      ),
+      body = dashboardBody(
+        # Call shinyCatch
+        spsDepend("shinyCatch"),
+        # Use shinyJS
+        useShinyjs(),
+
+        # Main content tabs
+        tabItems(
+          tabItem(
+            tabName = "Download",
+            h2("Data Download"),
+            fluidRow(
+              column(width = 12, areaSelectionUI("area"))
+            ),
+            fluidRow(
+              column(width = 12, parameterSelectionUI("params", parameter_names))
+            ),
+            fluidRow(
+              column(width = 12, dataDownloadUI("download"))
+            ),
+            uiOutput("summary_section"),
+            fluidRow(
+              column(width = 12, downloadHandlersUI("download_handlers"))
+            )
+          )
+        )
+      )
     )
   )
 }
